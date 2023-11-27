5d29d0955aab55bfd6c77c98f2ee0595276b898b
Clyde-Goodall <goodalluigi@gmail.com>
2023-11-26 20:47:08 -0500

    some more odds n ends to test websocket

--- utility/actions.py
+++ utility/actions.py
@@ -2,8 +2,10 @@ import alpaca_trade_api as alpaca
 
 # alpaca portfolio/rebalance actions
 
-def triggered_buy(ticker, amt):
+def triggered_buy(ticker, amt, order_type="mkt"):
+    # todo
     return
 
-def triggered_sell(ticker, amt):
+def triggered_sell(ticker, amt, order_type="mkt"):
+    # todo
     return
